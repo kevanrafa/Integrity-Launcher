592f5be1025aeadb8b7dc78590b49f83fa390757
kevanrafa <kevanrafa10@gmail.com>
2026-04-10 22:27:31 +0700

    YEs

--- build_windows.bat
+++ build_windows.bat
@@ -3,6 +3,8 @@ setlocal enabledelayedexpansion
 
 if "%1"=="" (
     echo Missing version argument
+    echo Usage: build_windows.bat [version]
+    echo Example: build_windows.bat 1.2.3
     exit /b 1
 )
 
@@ -10,11 +12,12 @@ set version=%1
 set version=%version:#v=%
 set PANDORA_RELEASE_VERSION=%version%
 
+echo Building version: %version%
+
 cargo build --release --target x86_64-pc-windows-msvc
 if errorlevel 1 exit /b 1
 
-strip target\x86_64-pc-windows-msvc\release\integrity_launcher.exe
-if errorlevel 1 exit /b 1
+:: Strip removed - skipping
 
 mkdir dist 2>nul
 
@@ -30,9 +33,7 @@ if defined CARGO_PACKAGER_SIGN_PRIVATE_KEY (
     set SAVED_KEY=!CARGO_PACKAGER_SIGN_PRIVATE_KEY!
     set CARGO_PACKAGER_SIGN_PRIVATE_KEY=
 )
-
-cargo packager --config { "name": "integrity-launcher", "outDir": "./dist", "productName": "Integrity Launcher", "version": "%version%", "identifier": "com.integrity.launcher", "resources": [], "authors": ["Moulberry"], "binaries": [{ "path": "IntegrityLauncher-Windows-x86_64.exe", "main": true }], "icons": ["package/windows.ico"] }
-
+cargo packager --config "{\"name\":\"integrity-launcher\",\"outDir\":\"./dist\",\"productName\":\"Integrity Launcher\",\"version\":\"%version%\",\"identifier\":\"com.integrity.launcher\",\"resources\":[],\"authors\":[\"Moulberry (Former), Kevanrafa10\"],\"binaries\":[{\"path\":\"IntegrityLauncher-Windows-x86_64.exe\",\"main\":true}],\"icons\":[\"package/windows.ico\"]}"
 :: Restore the environment variable if it was set
 if defined SAVED_KEY (
     set CARGO_PACKAGER_SIGN_PRIVATE_KEY=!SAVED_KEY!
@@ -47,22 +48,17 @@ move /Y "dist\IntegrityLauncher-Windows-x86_64_%version%_x64-setup.exe" dist\Int
 if errorlevel 1 exit /b 1
 
 if not "%CARGO_PACKAGER_SIGN_PRIVATE_KEY%"=="" (
+    echo Signing executable...
     cargo packager signer sign dist\IntegrityLauncher-Windows-x86_64-Portable.exe
     if errorlevel 1 exit /b 1
 
     :: Get file size in bytes
     for %%A in (dist\IntegrityLauncher-Windows-x86_64-Portable.exe) do set size=%%~zA
 
-    :: Get SHA1 hash (using certutil or PowerShell)
-    set sha1=
-    for /f "tokens=1" %%A in ('certutil -hashfile dist\IntegrityLauncher-Windows-x86_64-Portable.exe SHA1 ^| findstr /v "hash" ^| findstr /v "CertUtil"') do (
-        if not defined sha1 set sha1=%%A
-    )
-    if "!sha1!"=="" (
-        for /f "tokens=1" %%A in ('powershell -command "Get-FileHash -Algorithm SHA1 dist\IntegrityLauncher-Windows-x86_64-Portable.exe | ForEach-Object { $_.Hash.ToLower() }"') do set sha1=%%A
-    )
+    :: Get SHA1 hash using PowerShell
+    for /f "delims=" %%A in ('powershell -command "Get-FileHash -Algorithm SHA1 dist\IntegrityLauncher-Windows-x86_64-Portable.exe | ForEach-Object { $_.Hash.ToLower() }"') do set sha1=%%A
 
-    :: Get signature content (base64 encode the .sig file)
+    :: Get signature content
     set sig=
     if exist dist\IntegrityLauncher-Windows-x86_64-Portable.exe.sig (
         for /f "usebackq delims=" %%A in ("dist\IntegrityLauncher-Windows-x86_64-Portable.exe.sig") do set sig=%%A
@@ -87,6 +83,12 @@ if not "%CARGO_PACKAGER_SIGN_PRIVATE_KEY%"=="" (
     del dist\*.sig 2>nul
 )
 
-endlocal
+echo Build complete!
+echo Output files are in the 'dist' folder:
+echo   - dist\IntegrityLauncher-Windows-x86_64-Portable.exe
+echo   - dist\IntegrityLauncher-Windows-x86_64-Setup.exe
+if not "%CARGO_PACKAGER_SIGN_PRIVATE_KEY%"=="" (
+    echo   - dist\update_manifest_windows.json
+)
 
-pause
+endlocal
